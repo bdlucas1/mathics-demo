4c1ee6a56fcc8eb0dbfd769ab252ad911690cc3d
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-11-28 07:53:35 -0500

    Update fe_panel.py

--- fe_panel.py
+++ fe_panel.py
@@ -1,5 +1,8 @@
 import panel as pn
+import panel.widgets as pnw
 import plotly.express as px
+import plotly.graph_objects as go
+import ipywidgets as ipw
 
 import core
 import sym
@@ -20,9 +23,38 @@ fe = FE()
 # layout for epxpression
 layout = lt.expression_to_layout(fe, expr)
 
-# wrap in panel widget
-app = pn.Column(layout)
+#################
 
-# serve it forth
+
+# Text input widget
+text_input = pn.widgets.TextInput(
+    name="Data",
+    placeholder="Enter numbers, then press Enter",
+    value = "Plot3D[Cos[x] Sin[y], {x,0,4 Pi}, {y,0, 6 Pi}]"
+)
+
+# Output pane
+#plot_pane = pn.pane.Plotly(height=400)
+plot_pane = pn.pane.IPyWidget(ipw.HBox(), height=400)
+#help(pn.pane.IPyWidget)
+
+# Callback that runs whenever Enter commits the new value
+def update_plot(event):
+    expr = text_input.value
+    expr = session.parse(expr)
+    expr = expr.evaluate(session.evaluation)
+    layout = lt.expression_to_layout(fe, expr)
+
+    plot_pane.object = layout
+
+# Trigger update when value changes (Enter key)
+text_input.param.watch(update_plot, "value")
+
+# Layout
+app = pn.Column(
+    text_input,
+    plot_pane,
+)
+
 app.servable()
 
